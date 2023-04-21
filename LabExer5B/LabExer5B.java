468e31b6351e09f29d72a4ce30ea0394f92b3a89
Jiei_Dux <anonymouslydangerous@protonmail.com>
2023-04-21 21:59:30 +0800

    Code Update

--- LabExer5B/LabExer5B.java
+++ LabExer5B/LabExer5B.java
@@ -12,82 +12,100 @@ public class LabExer5B {
 		// Questions
 		String Question[] = new String[10];
 
-		Question[0] = " What is the color of the Sun.";
-		Question[1] = " What is the smallest planet in our solar system ( also known as Dwarf Planet )?";
-		Question[2] = " What is Japan's currency?";
-		Question[3] = " What do you call a magic owl?";
-		Question[4] = "";
-		Question[5] = "";
-		Question[6] = "";
-		Question[7] = "";
-		Question[8] = "";
-		Question[9] = "";
+		Question[0] = " What is the color of the Sun? ";
+		Question[1] = " What is the smallest planet in our solar system ( also known as Dwarf Planet )? ";
+		Question[2] = " What is Japan's currency? ";
+		Question[3] = " What do you call a magic owl? ";
+		Question[4] = " What filesystem does most Linux distributions uses by default? ";
+		Question[5] = "  ";
+		Question[6] = "  ";
+		Question[7] = "  ";
+		Question[8] = "  ";
+		Question[9] = "  ";
 
 		// Answers
 		String Choice1[] = {
-				" Yellow", // 1
-				"", // 2
-				"", // 3
-				"", // 4
-				"", // 5
-				"", // 6
-				"", // 7
-				"", // 8
-				"", // 9
-				"", // 10
+				" Yellow ", // 1
+				" Mercury ", // 2
+				"  ", // 3
+				"  ", // 4
+				"  ", // 5
+				"  ", // 6
+				"  ", // 7
+				"  ", // 8
+				"  ", // 9
+				"  ", // 10
 		};
 		String Choice2[] = {
-				" Red", // 1
-				"", // 2
-				"", // 3
-				"", // 4
-				"", // 5
-				"", // 6
-				"", // 7
-				"", // 8
-				"", // 9
-				"", // 01
+				" Red ", // 1
+				" Pluto ", // 2
+				"  ", // 3
+				"  ", // 4
+				"  ", // 5
+				"  ", // 6
+				"  ", // 7
+				"  ", // 8
+				"  ", // 9
+				"  ", // 01
 		};
 		String Choice3[] = {
-				" Blue", // 1
-				"", // 2
-				"", // 3
-				"", // 4
-				"", // 5
-				"", // 6
-				"", // 7
-				"", // 8
-				"", // 9
-				"", // 10
+				" Blue ", // 1
+				" Venus ", // 2
+				"  ", // 3
+				"  ", // 4
+				"  ", // 5
+				"  ", // 6
+				"  ", // 7
+				"  ", // 8
+				"  ", // 9
+				"  ", // 10
 		};
 		String Answer[] = {
-				"Yellow", // 1
-				"", // 2
-				"", // 3
-				"", // 4
-				"", // 5
-				"", // 6
-				"", // 7
-				"", // 8
-				"", // 9
-				"", // 10
+				"A", // 1
+				"B", // 2
+				"B", // 3
+				"A", // 4
+				"B", // 5
+				"C", // 6
+				"C", // 7
+				"C", // 8
+				"B", // 9
+				"A", // 10
 		};
 
+		// Answers Cheat Sheet
+		/*
+		 * {
+		 * 1. A
+		 * 2. B
+		 * 3. B
+		 * 4. A
+		 * 5. B
+		 * 6. C
+		 * 7. C
+		 * 8. C
+		 * 9. B
+		 * 10. A
+		 * }
+		 */
 		// Variables
 		int x,
+				QuestionNumber = 1,
 				QuestionCounter = 0,
 				ScoreCounter = 0;
 
 		//
 		String DisplayPrompt = " Enter your answer: ";
-		String Indicator = " Question #" + QuestionCounter + 1 + ": \n\n";
 
 		do {
 
 			try {
 
+				// Variables
+				String Indicator = " Question #" + QuestionNumber + ": \n\n";
+
 				/* ~~~~ CLEAR TERMINAL / CONSOLE ~~~~ */
-				System.out.print( "\033[H\033[2J" );
+				System.out.print("\033[H\033[2J");
 				System.out.flush();
 
 				/* ~~~~ INDICATOR ~~~~ */
@@ -101,7 +119,6 @@ public class LabExer5B {
 					} catch (InterruptedException INTERRUPT) {
 
 						//
-
 					}
 
 				}
@@ -117,7 +134,6 @@ public class LabExer5B {
 					} catch (InterruptedException INTERRUPT) {
 
 						//
-
 					}
 
 				}
@@ -136,7 +152,6 @@ public class LabExer5B {
 					} catch (InterruptedException INTERRUPT) {
 
 						//
-
 					}
 
 				}
@@ -154,7 +169,6 @@ public class LabExer5B {
 					} catch (InterruptedException INTERRUPT) {
 
 						//
-
 					}
 
 				}
@@ -172,12 +186,11 @@ public class LabExer5B {
 					} catch (InterruptedException INTERRUPT) {
 
 						//
-
 					}
 
 				}
 
-				/* ~~~~ PROMT DISPLAYING #1 ~~~~ */
+				/* ~~~~ PROMPT DISPLAYING #1 ~~~~ */
 				System.out.print("\n\n");
 
 				for (x = 0; x < DisplayPrompt.length(); x++) {
@@ -190,7 +203,6 @@ public class LabExer5B {
 					} catch (InterruptedException INTERRUPT) {
 
 						//
-
 					}
 
 				}
@@ -198,7 +210,7 @@ public class LabExer5B {
 				String usrChoice = usrInput.nextLine();
 
 				/* ~~~~ ANSWER CHECKING #1 ~~~~~ */
-				if (usrChoice.equalsIgnoreCase("A") || usrChoice.equalsIgnoreCase("B")|| usrChoice.equalsIgnoreCase("C")) {
+				if (usrChoice.equalsIgnoreCase("A") | usrChoice.equalsIgnoreCase("B") || usrChoice.equalsIgnoreCase("C")) {
 					if (usrChoice.equalsIgnoreCase(Answer[QuestionCounter])) {
 						++ScoreCounter;
 					}
@@ -207,12 +219,12 @@ public class LabExer5B {
 			} catch (Exception ERROR) {
 
 				//
-
 			}
 
+			QuestionNumber++;
 			QuestionCounter++;
 
-		} while (QuestionCounter < 1);
+		} while (QuestionCounter < 4);
 
 		System.out.print(" Your score is " + ScoreCounter + " out of 10 ");
 
