252a25344fe29dde22b11ca0d37d285a398fd3d6
Jiei_Dux <anonymouslydangerous@protonmail.com>
2023-04-22 22:25:47 +0800

    Code Update

--- TDEE Calculator/TDEECalculator.java
+++ TDEE Calculator/TDEECalculator.java
@@ -131,6 +131,7 @@ public class TDEECalculator {
         
         try { 
             System.in.read();
+			usrInput.close();
         } catch ( IOException e ){}
 
         BMR_M = 88.362 + (13.397 * weight) + (4.799 * height) - (5.677 * age);
