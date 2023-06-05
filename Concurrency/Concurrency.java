76cf78f427e8a0ffe4f96a30a2b6df52b9a9bb50
dux <anonymouslydangerous@protonmail.com>
2023-06-05 10:06:09 +0800

    Initial Commit

---
+++ Concurrency/Concurrency.java
@@ -0,0 +1,61 @@
+import java.util.*;
+import static java.lang.System.*;
+
+public class Concurrency extends Thread {
+
+	public static void main(String[] args) {
+
+		// Thingamajigs //
+		Scanner usrInput = new Scanner(in);
+		Concurrency Thread1 = new Concurrency();
+		Concurrency Thread2 = new Concurrency();
+
+		// Scanner //
+		out.print("Enter First Thread's Name: ");
+		String FirstThreadName = usrInput.nextLine();
+		Thread1.setName(FirstThreadName);
+
+		out.print("Enter Second Thread's Name: ");
+		String SecondThreadName = usrInput.nextLine();
+		Thread2.setName(SecondThreadName);
+		usrInput.close();
+
+		// is New? //
+		out.println(Thread1.getName() + " is NEW");
+		out.println(Thread2.getName() + " is NEW");
+
+		// Starting Message //
+		out.println("\n\nStarting the threads...");
+
+		// START THREAD??? //
+		Thread1.start();
+		Thread2.start();
+
+		// SCHLEEP //
+		try {
+			Thread.sleep(1000);
+			out.println("\n\nAfter sleep...");
+		} catch (InterruptedException ERROR) {
+		}
+
+	}
+
+	public void run() {
+
+		// YES //
+		try {
+			Thread.sleep(500);
+			out.println(Thread.currentThread().getName() + "is RUNNABLE");
+		} catch (InterruptedException ERROR) {
+		}
+
+		// NO //
+		try {
+			Thread.sleep(500);
+			out.println(Thread.currentThread().getName() + "is TERMINATED");
+		} catch (InterruptedException ERROR) {
+		}
+
+	}
+
+}
